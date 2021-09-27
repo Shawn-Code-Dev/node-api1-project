65b450171dae2f9bb127739270a63da5da34f0a0
Shawn Rice <Shawn.CS@protonmail.com>
2021-09-27 15:50:58 -0400

    finished writing all req

--- api/server.js
+++ api/server.js
@@ -35,12 +35,51 @@ server.get('/api/users/:id', async (req, res) => {
 })
 
 // POST '/api/users' - creates a user using the information sent inside the request body
-
+server.post('/api/users', async (req, res) => {
+  try {
+    const { name, bio } = req.body
+    if (!name || !bio) {
+      res.status(400).json({ message: "Please provide name and bio for the user" })
+    } else {
+      const newUser = await User.insert({ name, bio })
+      res.status(201).json(newUser)
+    }
+  } catch (err) {
+    res.status(500).json({ message: "There was an error while saving the user to the database" })
+  }
+})
 
 // PUT '/api/users/:id' - updates the user with the specified id using data from the request body, then returns the modified user
-
+server.put('/api/users/:id', async (req, res) => {
+  try {
+    const { name, bio } = req.body
+    const { id } = req.params
+    const user = await User.findById(id)
+    if (!user) {
+      res.status(404).json({ message: "The user with the specified ID does not exist" })
+    } else if (!name || !bio) {
+      res.status(400).json({ message: "Please provide name and bio for the user" })
+    } else {
+      const updatedUser = await User.update(id, { name, bio })
+      res.json(updatedUser)
+    }
+  } catch (err) {
+    res.status(500).json({ message: "The user information could not be modified" })
+  }
+})
 
 // DELETE '/api/users/:id' - removes the user with the specified id and returns the deleted user
-
+server.delete('/api/users/:id', async (req, res) => {
+  try {
+    const deletedUser = await User.delete(req.params.id)
+    if (!deletedUser) {
+      res.status(404).json({ message: "The user with the specified ID does not exist" })
+    } else {
+      res.json(deletedUser)
+    }
+  } catch (err) {
+    res.status(500).json({ message: "The user could not be removed" })
+  }
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
