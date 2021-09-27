7d7848e746653d6c80fc3e90a4cc33bdde56c5cb
Shawn Rice <Shawn.CS@protonmail.com>
2021-09-27 15:29:20 -0400

    first two get requests written

--- api/server.js
+++ api/server.js
@@ -1,3 +1,46 @@
-// BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+// try {} catch (err) {}
+
+// GET '/api/users' - returns an array of users
+server.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find()
+    res.status(200).json(users)
+  } catch (err) {
+    res.status(500).json({
+      message: "The users information could not be retrieved"
+    })
+  }
+})
+
+// GET '/api/users/:id' - returns the user object with the specified ID
+server.get('/api/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const user = await User.findById(id)
+    if (!user) {
+      res.status(404).json({ message: "The user with the specified ID does not exist" })
+    } else {
+      res.status(200).json(user)
+    }
+  } catch (err) {
+    res.status(500).json({message: "The user information could not be retrieved"})
+  }
+})
+
+// POST '/api/users' - creates a user using the information sent inside the request body
+
+
+// PUT '/api/users/:id' - updates the user with the specified id using data from the request body, then returns the modified user
+
+
+// DELETE '/api/users/:id' - removes the user with the specified id and returns the deleted user
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
